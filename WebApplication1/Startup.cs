e3cf0bd2ab715cfacfd28cdc655f30652667ab50
Baris Ceyhan <Baris_Ceyhan_@hotmail.com>
2023-03-13 13:57:46 -0400

    initial

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -79,12 +79,11 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
             }
             else
             {
